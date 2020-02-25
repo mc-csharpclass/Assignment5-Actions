58856e52e1dc92b1c87b672efe7497be4036eb05
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-02-25 09:53:45 +0900

    added a new line at end of task.py file and whitespace after commas in tests.py file

--- tests.py
+++ tests.py
@@ -18,11 +18,11 @@ class TestCase(unittest.TestCase):
     def test_calc_circ_area(self):
         # test the function is accurate for a radius of 1
         expected = 3.14159
-        self.assertAlmostEqual(expected, task.calc_circle_area(1),5)
+        self.assertAlmostEqual(expected, task.calc_circle_area(1), 5)
 
         # test the function is accurate for a radius of 5
         expected2 = 78.53982
-        self.assertAlmostEqual(expected2, task.calc_circle_area(5),5)
+        self.assertAlmostEqual(expected2, task.calc_circle_area(5), 5)
 
 
 if __name__ == '__main__':
