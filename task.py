2c1789b4cc80187b8d0bdbea9f513da0710d847a
mc-csharpclass <36938152+mc-csharpclass@users.noreply.github.com>
2020-02-25 09:48:41 +0900

    added calc_circle_area to task.py and added unit tests for that function in tests.py

--- task.py
+++ task.py
@@ -1,5 +1,15 @@
 # task file
 
+import math
+
 
 def firstrun():
     return "success"
+
+
+# this fucntion takes the radius of a circle and
+# returns the area
+def calc_circle_area(radius):
+
+    area = math.pi * math.pow(radius, 2)
+    return area
--- tests.py
+++ tests.py
@@ -15,6 +15,15 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test_calc_circ_area(self):
+        # test the function is accurate for a radius of 1
+        expected = 3.14159
+        self.assertAlmostEqual(expected, task.calc_circle_area(1),5)
+
+        # test the function is accurate for a radius of 5
+        expected2 = 78.53982
+        self.assertAlmostEqual(expected2, task.calc_circle_area(5),5)
+
 
 if __name__ == '__main__':
     unittest.main()
